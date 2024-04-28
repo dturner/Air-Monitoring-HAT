4ddb39ee87cabbf0b7bc10c7728b257fb78ec4f8
Don Turner <donaldpturner@gmail.com>
2024-04-28 23:01:48 +0100

    Start service commands

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 ln -s /root/Air-Monitoring-HAT/air-quality-monitor.service /etc/systemd/system/air-quality-monitor.service
+
+systemctl daemon-reload
+systemctl enable air-quality-monitor.service
+systemctl restart air-quality-monitor.service
